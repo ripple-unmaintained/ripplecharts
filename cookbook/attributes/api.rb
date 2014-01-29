1e8793b1bf2ac3ff79e52e607c45032684e491fd
Justin Lynn <j@jaesharp.com>
2014-01-28 17:19:17 -0800

    deploys correctly

---
+++ cookbook/attributes/api.rb
@@ -0,0 +1,7 @@
+default['ripple']['ripplecharts']['api']['app']['port'] = 5993
+
+default['ripple']['ripplecharts']['api']['app']['db']['username'] = 'CHANGEME'
+default['ripple']['ripplecharts']['api']['app']['db']['password'] = 'CHANGEME'
+default['ripple']['ripplecharts']['api']['app']['db']['host'] = 'localhost'
+default['ripple']['ripplecharts']['api']['app']['db']['port'] = 5984
+default['ripple']['ripplecharts']['api']['app']['db']['database_name'] = 'CHANGEME'
--- cookbook/recipes/api.rb
+++ cookbook/recipes/api.rb
@@ -1,3 +1,8 @@
+include_recipe 'ripple-ripplecharts::default'
+
+configuration = node['ripple']['ripplecharts']['api']['app']
+database_configuration = configuration['db']
+
 application "ripple-ripplecharts-api" do
   path "/srv/ripple-ripplecharts-api"
   owner "www-data"
@@ -6,10 +11,34 @@ application "ripple-ripplecharts-api" do
   packages ['git']
 
   repository 'git://github.com/ripple/ripplecharts.git'
-  revision 'v2.0' # head of v2.0
+  revision 'v2.0' # head of v2.0 branch
 
   nodejs do
     npm 'api'
     entry_point "node_modules/ripplecharts-api/app.js"
   end
+
+  before_deploy do
+
+    file "/srv/ripple-ripplecharts-api/shared/apiConfig.json" do
+      owner 'root'
+      group 'www-data'
+      mode "0640" # owner read/write, group read, world none
+      content({
+        :port => configuration['port'],
+        :couchdb => {
+          :username => database_configuration['username'],
+          :password => database_configuration['password'],
+          :host => database_configuration['host'],
+          :port => database_configuration['port'],
+          :database => database_configuration['database_name']
+        }
+      }.to_json)
+    end
+
+  end
+
+  symlinks({
+    "apiConfig.json" => "node_modules/ripplecharts-api/apiConfig.json"
+  })
 end
